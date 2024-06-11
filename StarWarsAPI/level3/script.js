e868e7ca1bfe521854bd6614aae6668ca3c13133
Ali-Asheer <ali_asheer@hotmail.com>
2024-06-12 01:30:16 +0200

    Update script.js

--- StarWarsAPI/level3/script.js
+++ StarWarsAPI/level3/script.js
@@ -1,1 +1,23 @@
+const helpMeButton = document.querySelector('#help-me-button');
+const displayWho = document.querySelector('.results');
+helpMeButton.addEventListener('click', getInformation);
 
+async function getInformation() {
+  const inputWho = document.querySelector('#input-Who');
+  const baseUrl = 'https://swapi.dev/api/planets/?search=' + inputWho.value;
+  const settings = { method: 'GET' };
+  const response = await fetch(baseUrl, settings);
+  const data = await response.json();
+  const firstInfo = data;
+  const theInfo1 = firstInfo.results;
+  const theInfo2 = firstInfo.count;
+ 
+  let text = '';
+  for (let i = 0; i < theInfo2; i++) {
+    for (let [x1, x2] of Object.entries(theInfo1[i])) {
+      text += x1 + ' : ' + x2 + '\n';
+    }
+    text += '\n'+'---------------------------------------------------\n\n'
+  }
+  displayWho.innerText = text+'Found: '+theInfo2
+}
